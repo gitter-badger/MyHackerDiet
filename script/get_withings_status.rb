8fcec5299be42513222de8e49c3230d2c7ce7c6a
Jon Gaudette <jon@digital-drip.com>
2011-05-23 10:18:47 -0400

    Attempt subscription for those who do not have it

--- script/get_withings_status.rb
+++ script/get_withings_status.rb
@@ -6,8 +6,18 @@ User.all.each do |usr|
 
   status = wu.notify_get 'http://myhackerdiet.com/withings'
 
-  if status == 247
-    puts "FAILED: #{usr.id}\t#{usr.email}\t#{usr.withings_userid}\t#{usr.withings_publickey}"
+  if status.class == Fixnum
+    puts "FAILED (#{status}): #{usr.id}\t#{usr.email}\t#{usr.withings_userid}\t#{usr.withings_publickey}"
+
+    begin
+            wu.user_update 1
+            wu.notify_subscribe 'http://myhackerdiet.com/withings', 'MyHackerDiet Withings'
+
+            status = wu.notify_get 'http://myhackerdiet.com/withings'
+            puts "\t\t\t\tSUBSCRIBED: #{Time.at(status.expires)}\t#{status.comment}"
+    rescue
+            puts "\t\t\t\tFAILED SUBSCRIBE"
+    end
   else
     puts "#{usr.id}\t#{usr.email}\t#{usr.withings_userid}\t#{usr.withings_publickey}\t#{Time.at(status.expires)}\t#{status.comment}"
   end

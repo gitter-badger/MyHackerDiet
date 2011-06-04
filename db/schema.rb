fa2e96259aa28d192c80d4b61277bc320d102887
Jon Gaudette <jon@digital-drip.com>
2011-06-04 10:26:58 -0400

    Schema tag not updated for some reason

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110102161658) do
+ActiveRecord::Schema.define(:version => 20110523124931) do
 
   create_table "sessions", :force => true do |t|
     t.integer  "person_id"

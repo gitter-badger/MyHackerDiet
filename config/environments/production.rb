28cdfcc7b3da6286b3d16e854dc4bf40e8fb76b1
Jon Gaudette <jon@digital-drip.com>
2011-08-23 10:32:09 -0400

    Update production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,4 +48,13 @@ Mhd::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { :host => 'myhackerdiet.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "mail.digital-drip.com",
+    :port                 => 25,
+    :user_name            => 'jon',
+    :password             => 'MArasu9A',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end

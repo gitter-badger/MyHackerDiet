88b86b8c822a90f916a28cf6d29504f37302584d
Jon Gaudette <jon.gaudette@corista.com>
2014-08-29 07:52:25 -0400

    new config

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,13 +29,13 @@ set :log_level, :info
 # set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 10
 
 namespace :deploy do
 

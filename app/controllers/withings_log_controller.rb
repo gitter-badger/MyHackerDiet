583473fef1bd6724db303579bb9611be05b0fa65
Jon Gaudette <jon.gaudette@corista.com>
2014-08-29 14:21:08 -0400

    Do not show all users withings history

--- app/controllers/withings_log_controller.rb
+++ app/controllers/withings_log_controller.rb
@@ -1,7 +1,7 @@
 class WithingsLogController < ApplicationController
   def index
-    @logs = WithingsLog.where(:user_id == current_user.withings_userid).order('sdate DESC')
-    @withings_logs = Withings.where(:user_id == current_user.withings_userid).order('rec_date DESC').limit(200)
+    @logs = WithingsLog.where("userid = ?", current_user.withings_userid).order('sdate DESC').limit(50)
+    @withings_logs = Withings.where("userid = ?", current_user.withings_userid).order('rec_date DESC').limit(50)
 
     respond_to do |format|
       format.html
